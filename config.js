9766b654872da8a2048b0aebdc47bce3b8de226f
baiye071 <2042336258@qq.com>
2026-05-21 01:23:33 +0800

    Move API key to user input, remove from config.js

--- config.js
+++ config.js
@@ -1,5 +1,11 @@
 const API_CONFIG = {
   baseUrl: 'https://token-plan-cn.xiaomimimo.com',
-  apiKey: 'tp-c3ylikxbmhacmczgcjrhrgw8kxp8340s3ywaxunkzzpnaiit',
   model: 'mimo-v2.5-pro'
 };
+
+function getApiKey() {
+  return localStorage.getItem('mimo_api_key') || '';
+}
+function setApiKey(key) {
+  localStorage.setItem('mimo_api_key', key);
+}
